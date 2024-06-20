abe53fd695f0772e0ebe04152e76a8cb4da80f64
wasimikbal <44136359+wasimikbal@users.noreply.github.com>
2024-06-20 13:39:45 +0500

    Update FooterBanner.tsx

--- app/components/FooterBanner.tsx
+++ app/components/FooterBanner.tsx
@@ -20,9 +20,9 @@ const FooterBanner:React.FC<Banner> = ({...bannerProps}) => {
           <p>{smallText}</p>
           <h3>{midText}</h3>
           <p>{desc}</p>
-          {/* <Link href={`/product/${product}`}> */}
+          <Link href={`/product/${product.toLowerCase()}`}>
             <button type='button'>{buttonText}</button>
-          {/* </Link> */}
+          </Link>
         </div>
         <img src={urlFor(image).toString()} className='footer-banner-image'></img>
       </div>
